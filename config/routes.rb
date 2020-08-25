7d1e93a87a47707e004a18eee40811514e6b0804
Ahsan Akbar <ahsanakbar252@gmail.com>
2020-08-25 21:21:16 +0500

    Create user page created

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'letsgo/home',to:'letsgo#home'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
